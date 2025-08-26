3585a8295a2d1416de2a15f3816b1d20ba49b774
mazayaaa <fatharanimazayaap@gmail.com>
2025-08-26 16:52:34 +0700

    ✨ feat(seeder): add UsersTableSeeder for demo accounts

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Admin Demo',
+                'email' => 'admin@demo.test',
+                'password' => Hash::make('123456789'),
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'Staf Demo',
+                'email' => 'staf@demo.test',
+                'password' => Hash::make('123456789'),
+                'role' => 'staf',
+            ],
+            [
+                'name' => 'Kepala Unit Demo',
+                'email' => 'kepunit@demo.test',
+                'password' => Hash::make('123456789'),
+                'role' => 'kepala_unit',
+            ],
+            [
+                'name' => 'Katimker Demo',
+                'email' => 'katimker@demo.test',
+                'password' => Hash::make('123456789'),
+                'role' => 'katimker',
+            ],
+            [
+                'name' => 'Kabid Demo',
+                'email' => 'kabid@demo.test',
+                'password' => Hash::make('123456789'),
+                'role' => 'kabid',
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::updateOrCreate(
+                ['email' => $user['email']],
+                $user
+            );
+        }
+    }
+}
